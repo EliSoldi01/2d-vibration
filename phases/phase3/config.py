d0f6a6fc08f9e278674ce725b18926e0556f40f2
Elisa_Soldi <82680008+EliSoldi01@users.noreply.github.com>
2026-03-12 14:10:32 +0100

    Setup ready for phase3

---
+++ phases/phase3/config.py
@@ -0,0 +1,28 @@
+﻿ARM        = "right"
+START_CELL = [7, 10]
+
+DATA_PATH     = "C:/Users/Utente/Desktop/Elisa/Research/2D-vibration/Phase3-Trajectory/Results/data_all_subjects_P3.xlsx"
+SUBJECTS_PATH = "C:/Users/Utente/Desktop/Elisa/Research/2D-vibration/Phase3-Trajectory/Results/Subjects/Subjects_list_P3.xlsx"
+PROTOCOL_PATH = "phases/phase3/protocol2.json"
+OUTPUT_ROOT   = "results/phase3"
+
+# --- Cosa eseguire ---
+DO_HEATMAPS                              = True
+DO_REGRESSIONS                           = True
+DO_REGRESSION_ORIGINAL_DURATIONS_SUBJ   = True
+DO_REGRESSION_ORDERED_DURATIONS_SUBJ    = False
+DO_REGRESSION_ORIGINAL_DURATIONS_GLOBAL = True
+DO_REGRESSION_ORDERED_DURATIONS_GLOBAL  = False
+DO_STD_EXCEL                             = False
+DO_MODEL_PARAMETERS                      = True
+DO_SIGMOID_FIT                           = True
+DO_CROSS_VALIDATION                      = True
+
+# --- Opzioni aggiuntive ---
+RECALC_SUBJECT      = True
+UPDATE_GROUP_AVERAGE = True
+SAVE_PLOTS          = True
+
+# --- Filtri ---
+SUBJECTS_TO_PROCESS = ["S01"]
+PATTERNS_TO_PROCESS = ["001_000", "000_001"]
